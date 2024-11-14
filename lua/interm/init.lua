11457acd550f2cb09015b53eee846e3fe4a3670a
Benjamin Lobos <blobosle@purdue.edu>
2024-11-13 22:01:24 -0500

    I27;

--- lua/interm/init.lua
+++ lua/interm/init.lua
@@ -2,6 +2,12 @@
 
 local M = require("interm.term")
 
+
+function M.setup()
+    -- Plugin setup code
+    print("interm.nvim setup called")
+end
+
 vim.api.nvim_set_keymap('n', 'Q', ':lua require("interm.term").cd_and_open_term()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<C-q>', [[<C-\><C-n>i exit<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>q', ':lua require("interm.term").cd_and_open_term_mod()<CR>', { noremap = true, silent = true })
