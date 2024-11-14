dff1d0c794defc862e6f95ee5e69fd24b73089d0
Benjamin Lobos <blobosle@purdue.edu>
2024-11-13 21:57:05 -0500

    Updated files

--- lua/interm/init.lua
+++ lua/interm/init.lua
@@ -1,6 +1,6 @@
 -- init.lua
 
-require("interm.term")
+local M = require("interm.term")
 
 vim.api.nvim_set_keymap('n', 'Q', ':lua require("interm.term").cd_and_open_term()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<C-q>', [[<C-\><C-n>i exit<CR>]], { noremap = true, silent = true })
@@ -8,3 +8,5 @@ vim.api.nvim_set_keymap('n', '<leader>q', ':lua require("interm.term").cd_and_op
 vim.api.nvim_set_keymap('n', '<S-CR>', '<C-w>w', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<S-CR>', '<C-\\><C-n><C-w>w', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-N>', { noremap = true, silent = true })
+
+return M
