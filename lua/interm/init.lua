627e7e0b001b18d36315fb58fb7e62dbb08eb60c
Benjamin Lobos <blobosle@purdue.edu>
2024-11-13 19:02:04 -0500

    Minor fixes

--- lua/interm/init.lua
+++ lua/interm/init.lua
@@ -4,7 +4,7 @@
 require("interm.term")
 
 -- Key mappings
-vim.api.nvim_set_keymap('n', 'Q', ':lua require("mytermplugin.term").cd_and_open_term()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>q', ':lua require("mytermplugin.term").cd_and_open_term_mod()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'Q', ':lua require("interm.term").cd_and_open_term()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>q', ':lua require("interm.term").cd_and_open_term_mod()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('t', '<C-q>', [[<C-\><C-n>i exit<CR>]], { noremap = true, silent = true })
 
